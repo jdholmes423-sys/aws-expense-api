c93a89e7087df25c89a8e8f8b1a80d37a72d6e5a
Jamil Holmes <jdholmes423@gmail.com>
2026-09-10 12:52:32 -0400

    Create DynamoDB expenses table

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,11 @@
+resource "aws_dynamodb_table" "expenses" {
+  name           = "Expenses"
+  billing_mode   = "PAY_PER_REQUEST"
+  
+  hash_key       = "expense_id"
+  
+ attribute {
+    name = "expense_id"
+    type = "S"
+  }
+}
